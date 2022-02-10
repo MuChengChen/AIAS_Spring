336ce133b543ef004394abca8a03e28bfbe8ac6d
xuann0118 <xuann.cn@gmail.com>
2022-02-10 15:53:41 +0800

    hw3-3 bug fixed

--- HW3-3/main.c
+++ HW3-3/main.c
@@ -1,45 +1,58 @@
 //main.c
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4
+#define SIZE 16
 
-void print_sudoku(char *data) {
-    int i,j;
+char test_c_data[16] = { 0, 0, 2, 0, 
+                         0, 0, 0, 4,
+                         2, 3, 0, 0, 
+                         0, 4, 0, 0 };
+                      
+char test_asm_data[16] = { 0, 0, 2, 0, 
+                           0, 0, 0, 4,
+                           2, 3, 0, 0, 
+                           0, 4, 0, 0 };
+
+void print_sudoku_result() {
+    int i;
+    printf("\nutput c & assembly function result\n");
+    printf("c result :\n");
+    for( i=0 ; i<SIZE ; i++) {   
+        printf("%d ", *(test_c_data+i));
+    }
+
+    printf("\nassembly result :\n");
     for( i=0 ; i<SIZE ; i++) {
-        for( j=0 ; j<SIZE ; j++) {
-            printf("%d ", *(data+j+4*i));
+        printf("%d ", *(test_asm_data+i));
+    }
+
+    int flag = 0;
+    for( i=0 ; i<SIZE ; i++) {
+        if (*(test_c_data+i) != *(test_asm_data+i)) {
+            flag = 1;
+            break;
         }
+    }
+
+    if (flag == 1){
+        printf("\nyour c & assembly got different result!\n");
     }
 }
 
 
-int sudoku_2x2_c(char *data){
+void sudoku_2x2_c(char *test_c_data){
 
 // TODO
 // Finish your sudoku algorithm in c language
 
 }
 
-int sudoku_2x2_asm(char *data); // TODO, sudoku_2x2_asm.S
+void sudoku_2x2_asm(char *test_asm_data); // TODO, sudoku_2x2_asm.S
 
- 
-char *test_c_data = { 0, 0, 2, 0, 
-                      0, 0, 0, 4,
-                      2, 3, 0, 0, 
-                      0, 4, 0, 0 };
-                      
-char *test_asm_data = { 0, 0, 2, 0, 
-                        0, 0, 0, 4,
-                        2, 3, 0, 0, 
-                        0, 4, 0, 0 };
                         
 int main() {
-    if (sudoku_2x2_c(test_c_data)) {
-        print_sudoku(test_c_data);
-    }
-    
-    if (sudoku_2x2_asm(test_asm_data)) {
-        print_sudoku(test_asm_data);
-    }
+    sudoku_2x2_c(test_c_data);
+    sudoku_2x2_asm(test_asm_data);
+    print_sudoku_result();
     return 0;
-} 
+}
