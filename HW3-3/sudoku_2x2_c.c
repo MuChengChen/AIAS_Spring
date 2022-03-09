4a010f270524a2761f06cda3f89db740b7b18af9
Oscar <e94096097@gs.ncku.edu.tw>
2022-03-09 22:00:46 +0800

    On branch master  Changes to be committed: 	modified:   sudoku_2x2_c.c

--- HW3-3/sudoku_2x2_c.c
+++ HW3-3/sudoku_2x2_c.c
@@ -53,9 +53,11 @@ bool solve(char *test_c_data,int index){
 		return true;
 	}
 	if(*(test_c_data+index)>0){
+		//if the block is not 0,skip this block
 		return solve(test_c_data,index+1);
 	}
 	else{
+		//check which number can set in the block
 		for(i=1;i<=4;i++){
 			*(test_c_data+index)=i;
 			if(check(test_c_data,index)&& solve(test_c_data,index+1)){
