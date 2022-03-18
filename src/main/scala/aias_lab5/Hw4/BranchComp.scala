dc4b2c8cf91c09977ea640f5e32f4f90451cab8f
Wei-Kai Hsu <N26104890@gs.ncku.edu.tw>
2022-03-18 17:07:37 +0800

    Commit Message 1. Delete the BranchComp.scala cuz it doesn't needed in this lab

--- src/main/scala/aias_lab5/Hw4/BranchComp.scala
+++ src/main/scala/aias_lab5/Hw4/BranchComp.scala
@@ -4,19 +4,19 @@ import chisel3._
 import chisel3.util._
 
 object condition{
-  val EQ = "b000".U
-  val NE = "b001".U
-  val LT = "b100".U
-  val GE = "b101".U
-  val LTU = "b110".U
-  val GEU = "b111".U
+  val EQ = 0.U
+  val NE = 0.U
+  val LT = 0.U
+  val GE = 0.U
+  val LTU = 0.U
+  val GEU = 0.U
 }
 
 import condition._
 
 class BranchComp extends Module{
     val io = IO(new Bundle{
-        val en = Input(Bool())
+        val en = Input(Bool()) // when the inst is B-type
         val funct3 = Input(UInt(3.W))
         val src1 = Input(UInt(32.W))
         val src2 = Input(UInt(32.W))
@@ -24,25 +24,5 @@ class BranchComp extends Module{
         val brtaken = Output(Bool()) //for pc.io.brtaken
     })
 
-    val eq = WireDefault(false.B)
-    val lt = WireDefault(false.B)
-
-    when(io.funct3 === LTU || io.funct3 === GEU){
-      when(io.src1 < io.src2) { lt := true.B }
-    }.otherwise{
-      when(io.src1.asSInt === io.src2.asSInt) { eq := true.B }
-      when(io.src1.asSInt < io.src2.asSInt) { lt := true.B }
-    }
-    
     io.brtaken := false.B
-    
-    when(io.en){
-      when(io.funct3 === EQ &&  eq){io.brtaken := true.B}
-      when(io.funct3 === NE && !eq){io.brtaken := true.B}
-      when((io.funct3 === LT || io.funct3 === LTU) &&  lt){io.brtaken := true.B}
-      when((io.funct3 === GE || io.funct3 === GEU) && !lt){io.brtaken := true.B}
-    }
-    
-
-
 }
