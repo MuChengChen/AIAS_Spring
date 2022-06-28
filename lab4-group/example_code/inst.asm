2771bcab11b85e74168a2617d80d1e835847030d
Oscar <e94096097@gs.ncku.edu.tw>
2022-06-28 12:28:39 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   lab4-group/example_code/Hw3_inst.asm
    	modified:   lab4-group/example_code/inst.asm
    	modified:   lab4-group/example_code/m_code.hex

--- lab4-group/example_code/inst.asm
+++ lab4-group/example_code/inst.asm
@@ -13,4 +13,13 @@ binv x9, x3, x7
 bext x10, x3, x2
 ror x11, x3, x2
 rol x12, x3, x2
+addi x9, x0, 0xff
+addi x10, x0, 1
+addi x11, x0, -1
+rev8 x3, x9
+sh1add x5, x10, x0
+sh2add x6, x10, x0
+sh3add x7, x10, x0
+orc_d x4, x11
+zext_h x8, x11
 hcf
